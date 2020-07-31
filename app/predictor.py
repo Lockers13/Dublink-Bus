1241c7a330be1dd4b5a3ba8397632e5312261302
Lorcan Conroy <lorcan.conroy@ucdconnect.ie>
2020-07-31 23:07:16 +0100

    Minor tweaks to predictor script

--- app/predictor.py
+++ app/predictor.py
@@ -9,6 +9,7 @@ import sklearn
 import json
 
 
+
 def get_prediction(model, m_args, data_dir):
     def convert_secs(seconds): 
         seconds = seconds % (24 * 3600) 
@@ -18,8 +19,8 @@ def get_prediction(model, m_args, data_dir):
         seconds = round(seconds % 60)
         
         return {'hours': hour, 'minutes': minutes, 'seconds': seconds}
-    
 
+    
     json_str = json.dumps(
             {
                 "LINEID": m_args['lineid'],
@@ -37,14 +38,18 @@ def get_prediction(model, m_args, data_dir):
                 "weather_id": m_args['weather_id'],
             })
 
-        
     json_form = json.loads(json_str)
+
+
+
     df_query = pd.DataFrame([json_form])
+
 
     LINEID = str(df_query.loc[0]['LINEID'])
     SUBMIT_TIME = int(df_query['SUBMIT_TIME'])
     STOPOINTID1 = str(int(df_query['STOPPOINTID1']))
     STOPOINTID2 = str(int(df_query['STOPPOINTID2']))
+
 
     df_sequence = pd.read_csv(os.path.join(data_dir, 'stop_sequence/stop_{0}.csv'.format(m_args['lineid'])))
 
@@ -57,9 +62,14 @@ def get_prediction(model, m_args, data_dir):
 
     df_route_list = df_sequence.iloc[route_list].apply(lambda x: x.reset_index(drop=True))
 
-    DIRECTION = df_route_list.loc[0]['DIRECTION']
+    try:
+        DIRECTION = df_route_list.loc[0]['DIRECTION']
+    except:
+        pass
+        
 
     df_query = df_query.drop(['SUBMIT_TIME'],axis=1)
+
     df_query_stop1 = df_query.drop(['STOPPOINTID2'],axis=1)
     df_query_stop2 = df_query.drop(['STOPPOINTID1'],axis=1)
 
@@ -69,14 +79,16 @@ def get_prediction(model, m_args, data_dir):
 
     df_timetable = df_timetable.astype('str')
 
+
+
     df_timetable1 = df_timetable[df_timetable['STOPPOINTID']== STOPOINTID1]
     df_timetable1 = df_timetable1.rename({'STOPPOINTID': 'STOPPOINTID1'}, axis=1)
     df_timetable1 = df_timetable1.rename({'PROGRNUMBER': 'PROGRNUMBER1'}, axis=1)
 
+
     df_timetable2 = df_timetable[df_timetable['STOPPOINTID']== STOPOINTID2]
     df_timetable2 = df_timetable2.rename({'STOPPOINTID': 'STOPPOINTID2'}, axis=1)
     df_timetable2 = df_timetable2.rename({'PROGRNUMBER': 'PROGRNUMBER2'}, axis=1)
-
 
     df_timetable = pd.merge(df_timetable1, df_timetable2, on=['LINEID','ROUTEID','DIRECTION','TRIPS_PLANNEDTIME_DEP','DAYOFWEEK'])
 
@@ -84,8 +96,10 @@ def get_prediction(model, m_args, data_dir):
     df_timetable1 = df_timetable.drop(['STOPPOINTID2','PROGRNUMBER2'], axis=1)
     df_timetable2 = df_timetable.drop(['STOPPOINTID1','PROGRNUMBER1'], axis=1)
 
+
     df_timetable1 = df_timetable1.rename({'PROGRNUMBER1': 'PROGRNUMBER'}, axis=1)
     df_timetable2 = df_timetable2.rename({'PROGRNUMBER2': 'PROGRNUMBER'}, axis=1)
+
 
     df_fill_stop1 = pd.merge(df_timetable1, df_query_stop1, on=['LINEID','DAYOFWEEK','STOPPOINTID1'])
     df_fill_stop2 = pd.merge(df_timetable2, df_query_stop2, on=['LINEID','DAYOFWEEK','STOPPOINTID2'])
@@ -95,14 +109,12 @@ def get_prediction(model, m_args, data_dir):
     df_fill_stop2 = df_fill_stop2.drop(['STOPPOINTID2'],axis=1)
 
 
-
     df_fill_stop1_rev = df_fill_stop1.astype({'LINEID': 'category','ROUTEID':'category','PROGRNUMBER':'int64','DIRECTION':'category','TRIPS_PLANNEDTIME_DEP':'int64','DAYOFWEEK':'category','HOLIDAY':'category',"temp":"float64", "feels_like":"float64","pressure":"float64","humidity":"float64","wind_speed":"float64", "clouds_all":"float64","weather_id":"category"})
-
 
     df_fill_stop2_rev = df_fill_stop2.astype({'LINEID': 'category','ROUTEID':'category','PROGRNUMBER':'int64','DIRECTION':'category','TRIPS_PLANNEDTIME_DEP':'int64','DAYOFWEEK':'category','HOLIDAY':'category',"temp":"float64", "feels_like":"float64","pressure":"float64","humidity":"float64","wind_speed":"float64", "clouds_all":"float64","weather_id":"category"})
 
 
-    
+
     df_fill_stop1_rev = df_fill_stop1_rev.sort_values(by=['TRIPS_PLANNEDTIME_DEP'])
     df_fill_stop2_rev = df_fill_stop2_rev.sort_values(by=['TRIPS_PLANNEDTIME_DEP'])
 
@@ -110,11 +122,7 @@ def get_prediction(model, m_args, data_dir):
     df_fill_stop1_rev = df_fill_stop1_rev.apply(lambda x: x.reset_index(drop=True))
     df_fill_stop2_rev = df_fill_stop2_rev.apply(lambda x: x.reset_index(drop=True))
 
-
     departure_list = df_fill_stop1_rev['TRIPS_PLANNEDTIME_DEP'].tolist()
-
-
-    list(df_fill_stop1_rev.columns.values.tolist())
 
     df_fill_stop1_rev =  df_fill_stop1_rev[[
     'LINEID',
@@ -132,6 +140,7 @@ def get_prediction(model, m_args, data_dir):
     'clouds_all',
     'weather_id'
     ]]
+
 
     df_fill_stop2_rev =  df_fill_stop2_rev[[
     'LINEID',
@@ -154,11 +163,13 @@ def get_prediction(model, m_args, data_dir):
     fill_stop1_pred = model.predict(df_fill_stop1_rev)
     fill_stop2_pred = model.predict(df_fill_stop2_rev)
 
+
     for index, item in enumerate(fill_stop1_pred, start=0):
         if item > SUBMIT_TIME:
-            arrival_start_secs = item
-            arrival_end_secs = fill_stop2_pred[index]
-            break
+            if item > SUBMIT_TIME:
+                arrival_start_secs = item
+                arrival_end_secs = fill_stop2_pred[index]
+                break
 
     journey_info = {}
 
@@ -167,10 +178,11 @@ def get_prediction(model, m_args, data_dir):
         journey_info["arrival_startstop"] = convert_secs(arrival_start_secs)
         journey_info["arrival_endstop"] = convert_secs(arrival_end_secs)
         journey_info["journey_time"] = convert_secs(arrival_end_secs - arrival_start_secs)
-
         return journey_info
+        
     except Exception as e:
-        print(e)
         journey_info["error"] = "model error"
         return journey_info
 
+
+    
